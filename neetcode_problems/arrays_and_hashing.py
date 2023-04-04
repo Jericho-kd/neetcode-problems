9778010f7b66c88eb79e6092bf4ccc4a1d5b88c8
Anthony Sazonov <jericho9378@gmail.com>
2023-04-04 23:41:05 +0300

    feat: add 2 problem solutions with tests

--- neetcode_problems/arrays_and_hashing.py
+++ neetcode_problems/arrays_and_hashing.py
@@ -1,5 +1,4 @@
 #217. Contains Duplicate
-
 def contains_duplicate(nums: list[int]) -> bool:
     s: set[int] = set()
 
@@ -9,4 +8,21 @@ def contains_duplicate(nums: list[int]) -> bool:
         s.add(el)
 
     return False
+
+
+#1. Two Sum
+def two_sum(nums: list[int], target: int) -> list[int] | None:
+    tmp: dict[int, int] = {}
+
+    for idx, el in enumerate(nums):
+        if target - el in tmp.keys():
+            return [tmp[target-el], idx]
+        tmp[el] = idx
+
+
+#242. Valid Anagram
+def is_anagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
     
+    return sorted(s) == sorted(t)
--- tests/test_arrays_and_hashing.py
+++ tests/test_arrays_and_hashing.py
@@ -1,6 +1,17 @@
-from neetcode_problems.arrays_and_hashing import contains_duplicate
+import neetcode_problems.arrays_and_hashing as ah_problems
 
 def test_contains_duplicate():
-    assert contains_duplicate([1,2,3,1]) == True
-    assert contains_duplicate([1,2,3,4]) == False
-    assert contains_duplicate([1,1,1,3,3,4,3,2,4,2]) == True
+    assert ah_problems.contains_duplicate([1,2,3,1]) == True
+    assert ah_problems.contains_duplicate([1,2,3,4]) == False
+    assert ah_problems.contains_duplicate([1,1,1,3,3,4,3,2,4,2]) == True
+
+
+def test_two_sum():
+    assert ah_problems.two_sum([2,7,11,15], 9) == [0, 1]
+    assert ah_problems.two_sum([3,2,4], 6) == [1, 2]
+    assert ah_problems.two_sum([3, 3], 6) == [0, 1]
+
+
+def test_is_anagram():
+    assert ah_problems.is_anagram("anagram", "nagaram") == True
+    assert ah_problems.is_anagram("cat", "rat") == False
