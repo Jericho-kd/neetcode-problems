b8086fd0fbf4f906e277621552cfc9addbab115d
Anthony Sazonov <jericho9378@gmail.com>
2023-04-07 18:16:04 +0300

    feat: add 2 easy problem solutions with tests

--- neetcode_problems/bit_manipulation.py
+++ neetcode_problems/bit_manipulation.py
@@ -18,3 +18,31 @@ def hamming_weight(n: int) -> int:
         n >>= 1
     
     return count
+
+
+# 338. Counting Bits (easy)
+def count_bits(n: int) -> list[int]:
+    result: list[int] = []
+
+    for num in range(n+1):
+        count: int = 0
+
+        while num != 0:
+            if num & 1 == 1:
+                count += 1
+            num >>= 1
+        result.append(count)
+
+    return result
+
+
+#190. Reverse Bits (easy)
+def reverse_bits(n: int) -> int:
+    reversed_num: int = 0
+
+    for _ in range(32):
+        reversed_num <<= 1
+        reversed_num |=  (n & 1)
+        n >>= 1
+
+    return reversed_num
--- tests/test_bit_manipulation.py
+++ tests/test_bit_manipulation.py
@@ -11,3 +11,13 @@ def test_hamming_weight():
     assert bm_problems.hamming_weight(0o00000000000000000000000000001011) == 3
     assert bm_problems.hamming_weight(0o00000000000000000000000010000000) == 1
     assert bm_problems.hamming_weight(0o11111111111111111111111111111101) == 31
+
+
+def test_count_bits():
+    assert bm_problems.count_bits(2) == [0, 1, 1]
+    assert bm_problems.count_bits(5) == [0, 1, 1, 2, 1, 2]
+
+
+def test_reverse_bits():
+    assert bm_problems.reverse_bits(43261596) == 964176192
+    assert bm_problems.reverse_bits(4294967293) == 3221225471
