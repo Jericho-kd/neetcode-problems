95d1c0ecd8db202963eee2ccbd164fcd4b408858
Anthony Sazonov <jericho9378@gmail.com>
2023-04-09 23:42:19 +0300

    feat: add second solution for max_profit task

--- neetcode_problems/sliding_window.py
+++ neetcode_problems/sliding_window.py
@@ -1,4 +1,4 @@
-# 121. Best Time to Buy and Sell Stock
+# 121. Best Time to Buy and Sell Stock (easy)
 def max_profit(prices: list[int]) -> int:
         min_value, profit = float('inf'), 0
         
@@ -8,3 +8,17 @@ def max_profit(prices: list[int]) -> int:
             if i - min_value > profit:
                 profit = i - min_value
         return profit
+
+def max_profit_second(prices: list[int]) -> int:
+    left, right = 0, 1
+    max_profit = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            max_profit = max(profit, max_profit)
+        else:
+            left = right
+        right += 1
+
+    return max_profit
