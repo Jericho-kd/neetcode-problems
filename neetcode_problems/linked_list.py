0bca045f242ec8a0ed3632e05870c731efbd31f3
Anthony Sazonov <jericho9378@gmail.com>
2023-04-11 22:58:23 +0300

    feat: add 1 easy problem solution without tests
    Reverse list problem remains untested due to complicated mechanism of assert in pytest for LinkedList class

---
+++ neetcode_problems/linked_list.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ListNode:
+    value: int = field(repr=True, default=0)
+    next: ListNode | None = field(repr=True, default=None)
+
+
+# 206. Reverse Linked List (easy)
+def reverse_list(head: ListNode | None) -> ListNode | None:
+    reversed_list = None
+    current_head = head
+
+    while current_head:
+        new_head = current_head.next
+        current_head.next = reversed_list
+        reversed_list = current_head
+        current_head = new_head
+
+    return reversed_list
+        
+
+a = ListNode(1)
+b = ListNode(2)
+c = ListNode(3)
+d = ListNode(4)
+e = ListNode(5)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+print(reverse_list(a))
+
+# b.next = a
+# print(a.next, b.next)
+
