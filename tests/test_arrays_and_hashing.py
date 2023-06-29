767918a859a7eb248b3563300fe8aaf1197fdfba
Anthony Sazonov <jericho9378@gmail.com>
2023-06-29 23:36:14 +0300

    refactor: add parametrize decorator to all tests in test_arrays_and_hashing.py

--- tests/test_arrays_and_hashing.py
+++ tests/test_arrays_and_hashing.py
@@ -32,54 +32,74 @@ def test_group_anagrams(strs: list[str], result: list[list[str]]):
     assert ah_problems.group_anagrams(strs) == result
 
 
-def test_top_k_frequent():
-    assert ah_problems.top_k_frequent([1, 1, 1, 2, 2, 3], 2) == [1, 2]
-    assert ah_problems.top_k_frequent([1], 1) == [1]
+@tests.pytest.mark.parametrize("nums, k, result", 
+                               [([1, 1, 1, 2, 2, 3], 2, [1, 2]),
+                                ([1], 1, [1])])
+def test_top_k_frequent(nums: list[int], k: int, result: list[int]):
+    assert ah_problems.top_k_frequent(nums, k) == result
 
 
-def test_top_k_frequent_counter():
-    assert ah_problems.top_k_frequent_counter([1, 1, 1, 2, 2, 3], 2) == [1, 2]
-    assert ah_problems.top_k_frequent_counter([1], 1) == [1]
+@tests.pytest.mark.parametrize("nums, k, result", 
+                               [([1, 1, 1, 2, 2, 3], 2, [1, 2]),
+                                ([1], 1, [1])])
+def test_top_k_frequent_counter(nums: list[int], k: int, result: list[int]):
+    assert ah_problems.top_k_frequent_counter(nums, k) == result
 
 
-def test_product_except_self():
-    assert ah_problems.product_except_self([1, 2, 3, 4]) == [24, 12, 8, 6]
-    assert ah_problems.product_except_self([-1,1,0,-3,3]) == [0,0,9,0,0]
+@tests.pytest.mark.parametrize("nums, result",
+                               [([1, 2, 3, 4], [24, 12, 8, 6]),
+                                ([-1,1,0,-3,3], [0,0,9,0,0])])
+def test_product_except_self(nums: list[int], result: list[int]):
+    assert ah_problems.product_except_self(nums) == result
 
 
-def test_array_of_squares():
-    assert ah_problems.array_of_squares([-6, -5, -4, -3, -2, 1, 2, 3, 4]) == [1, 4, 4, 9, 9, 16, 16, 25, 36]
-    assert ah_problems.array_of_squares([-10000, -9999, -7, -5, 0, 0, 10000]) == [0, 0, 25, 49, 99980001, 100000000, 100000000]
-    assert ah_problems.array_of_squares([]) == []
+@tests.pytest.mark.parametrize("nums, result", 
+                               [([-6, -5, -4, -3, -2, 1, 2, 3, 4], [1, 4, 4, 9, 9, 16, 16, 25, 36]),
+                                ([-10000, -9999, -7, -5, 0, 0, 10000], [0, 0, 25, 49, 99980001, 100000000, 100000000]),
+                                ([], [])])
+def test_array_of_squares(nums: list[int], result: list[int]):
+    assert ah_problems.array_of_squares(nums) == result
 
 
-def test_search_matrix():
-    assert ah_problems.search_matrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 20) == False
-    assert ah_problems.search_matrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5) == True
+@tests.pytest.mark.parametrize("matrix, target, result", 
+                               [([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 20, False),
+                                ([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 5,  True)])
+def test_search_matrix(matrix: list[list[int]], target: int, result: bool):
+    assert ah_problems.search_matrix(matrix, target) == result
 
 
-def test_max_area():
-    assert ah_problems.max_area([1,8,6,2,5,4,8,3,7]) == 49
-    assert ah_problems.max_area([1, 1]) == 1
+@tests.pytest.mark.parametrize("height, result", 
+                               [([1,8,6,2,5,4,8,3,7], 49),
+                                ([1, 1], 1)])
+def test_max_area(height: list[int], result: int):
+    assert ah_problems.max_area(height) == result
 
 
-def test_roman_to_int():
-    assert ah_problems.roman_to_int("III") == 3
-    assert ah_problems.roman_to_int("LVIII") == 58
-    assert ah_problems.roman_to_int("MCMXCIV") == 1994
+@tests.pytest.mark.parametrize("s, result",
+                               [("III", 3),
+                                ("LVIII", 58),
+                                ("MCMXCIV", 1994)])
+def test_roman_to_int(s: str, result: int):
+    assert ah_problems.roman_to_int(s) == result
 
 
-def test_max_subarray():
-    assert ah_problems.max_subarray([-2,1,-3,4,-1,2,1,-5,4]) == 6
-    assert ah_problems.max_subarray([5,4,-1,7,8]) == 23
-    assert ah_problems.max_subarray([-1, -1, -1, -2]) == -1
+@tests.pytest.mark.parametrize("nums, result",
+                               [([-2,1,-3,4,-1,2,1,-5,4], 6),
+                                ([5,4,-1,7,8], 23),
+                                ([-1, -1, -1, -2], -1)])
+def test_max_subarray(nums: list[int], result: int):
+    assert ah_problems.max_subarray(nums) == result
 
 
-def test_remove_element():
-    assert ah_problems.remove_element([3,2,2,3], 3) == 2
-    assert ah_problems.remove_element([0,1,2,2,3,0,4,2], 2) == 5
+@tests.pytest.mark.parametrize("nums, val, result",
+                               [([3,2,2,3], 3, 2),
+                                ([0,1,2,2,3,0,4,2], 2, 5)])
+def test_remove_element(nums: list[int], val: int, result: int):
+    assert ah_problems.remove_element(nums, val) == result
 
 
-def test_longest_consecutive():
-    assert ah_problems.longest_consecutive([100,4,200,1,3,2]) == 4
-    assert ah_problems.longest_consecutive([0,3,7,2,5,8,4,6,0,1]) == 9
+@tests.pytest.mark.parametrize("nums, result", 
+                               [([100,4,200,1,3,2], 4), 
+                                ([0,3,7,2,5,8,4,6,0,1], 9)])
+def test_longest_consecutive(nums: list[int], result: int):
+    assert ah_problems.longest_consecutive(nums) == result
